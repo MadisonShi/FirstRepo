4fee090a273dc2168ad01886da40126371df87cc
Manqi Shi <Manqi@Manqis-Air-3.attlocal.net>
2020-09-01 11:56:51 -0500

    9.1 Scripts HW

---
+++ HW/HW5 Scripts/javascript/main.js
@@ -0,0 +1,6 @@
+function js_style(){
+     document.getElementById("text").style.color = "orange";
+     //document.getElementById("text").style.fontFamily = "Impact,Charcoal,sans-serif";
+     //document.getElementById("text").style.fontSize = "xx-small";
+     document.getElementById("text").style.font = "xx-small Impact,Charcoal,sans-serif";
+}
